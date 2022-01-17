b03ac7e60f67cbbd2da7be3dbcd72e33d6bac72a
Jan Hlaváč <jan.hlavac@protonmail.com>
2022-01-17 17:40:40 +0100

    potato

---
+++ TestApp/Controllers/PotatoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PotatoController : ControllerBase
+    {
+    }
+}
