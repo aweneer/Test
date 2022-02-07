b071b28982d5e0e537114610a86df73322a30862
Jan H <34675617+aweneer@users.noreply.github.com>
2022-02-07 01:18:20 +0100

    test

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -13,6 +13,7 @@ namespace TestApp
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("lol");
             CreateHostBuilder(args).Build().Run();
         }
 
